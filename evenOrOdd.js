8825581244a216649447bc2fbdf2cc5682b5acd7
User <yonelakennymakhubs@gmail.com>
2020-12-10 11:47:38 +0200

    Created frameSomeText file

---
+++ evenOrOdd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(num){
+    //If the devision remainder is not equal to zero when divided by 2 it wll printout "That is a odd number"
+    if (num % 2){
+      console.log("That is a odd number")
+    } 
+    //If the devision remainder is not equal to zero when divided by 2 it wll printout "That is a even number"
+    else{
+      console.log("That is a even number")
+    }
+  }
